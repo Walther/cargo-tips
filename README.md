c113a55527ce57344c68517a22502a34d4027288
Walther <veeti.haapsamo@gmail.com>
2020-09-18 18:33:08 +0300

    Initial MVP implementation of cargo tips

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # cargo-tips
+
+TIP: you can get tips while using cargo and rust!
+
+Original inspiration from [this tweet](https://twitter.com/lcnr7/status/1306886109461450752?s=20)
+
+> @lcnr7
+> how about adding loading screen tips to compilers?
+>
+> ```
+> ~/proj$ cargo build
+>  Compiling proj v0.1.0 (/home/programming/proj)
+>  Finished dev [unoptimized + debuginfo] target(s) in 0.34s
+>  Tip: use `deque.make_contiguous` to efficiently sort a `VecDeque` without allocating
+> ```
+
+## Usage
+
+- `cargo install tips` to install the binary
+- `tips -h` for up to date help & usage information
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use clap::Clap;
+use rand::seq::SliceRandom;
+
+#[derive(Clap)]
+#[clap(version = "0.1.0")]
+struct Opts {
+    /// Prints more verbose information used for debugging.
+    #[clap(short, long)]
+    debug: bool,
+}
+
+/// List of tips available in this program.
+/// Possible idea for future: categories of tips?
+const TIPS: &'static [&'static str] = &[
+    "I like you just the way you are",
+    "You look beautiful today",
+];
+
+fn main() {
+    let opts: Opts = Opts::parse();
+    if opts.debug {
+        println!("{} tips available", TIPS.len());
+    }
+
+    let mut rng = rand::thread_rng();
+    match TIPS.choose(&mut rng) {
+        Some(tip) => {
+            println!("TIP: {}", tip);
+        }
+        None => panic!("Could not find a tip to give you, sorry!"),
+    }
+}
